e486fef48094b161df56cfdf12751552719d4a12
mirceachira <chira.mircea.mc@gmail.com>
2019-07-16 11:26:45 +0300

    Moved statsd logging to new handlers.py, added default handler class

--- scrapy_statsd_extension/__init__.py
+++ scrapy_statsd_extension/__init__.py
@@ -1,9 +1,8 @@
-from scrapy_statsd_extension import utils, defaults
-
-import statsd
 from scrapy import signals
 from scrapy.exceptions import NotConfigured
-from twisted.internet import task
+from scrapy.utils.misc import load_object
+
+from scrapy_statsd_extension import utils, defaults
 
 
 class StatsdExtension(object):
@@ -12,14 +11,14 @@ class StatsdExtension(object):
         if not crawler.settings.getbool('STATSD_ENABLED', defaults.STATSD_ENABLED):
             raise NotConfigured
 
-        host = crawler.settings.get('STATSD_HOST', defaults.STATSD_HOST)
-        port = crawler.settings.get('STATSD_PORT', defaults.STATSD_PORT)
-
         self.log_periodic = crawler.settings.get(
-            'STATSD_LOG_PERIODIC', defaults.STATSD_LOG_PERIODIC)
+            'STATSD_LOG_PERIODIC', defaults.STATSD_LOG_PERIODIC
+        )
         self.callack_timer = crawler.settings.get(
-            'STATSD_LOG_EVERY', defaults.STATSD_LOG_EVERY)
-        self.client = statsd.StatsClient(host, port)
+            'STATSD_LOG_EVERY', defaults.STATSD_LOG_EVERY
+        )
+
+        self.handler = load_object(defaults.STATSD_HANDLER).from_crawler(crawler)
         self.stats = crawler.stats
 
     @classmethod
@@ -35,14 +34,13 @@ class StatsdExtension(object):
 
     def spider_opened(self, spider):
         if self.log_periodic:
-            self.log_task = task.LoopingCall(self.log_stats, spider)
+            self.log_task = utils.looping_call_task(self.log_stats, spider)
             self.log_task.start(self.callack_timer)
 
     def log_stats(self, spider):
         for key, value in self.stats.get_stats().items():
             if isinstance(value, int) or isinstance(value, float):
-                stat_key = utils.create_stat_key(key)
-                self.client.incr(stat_key, value)
+                self.handler.increment(utils.create_stat_key(key), value)
 
     def spider_closed(self, spider):
         if hasattr(self, 'log_task') and self.log_task.running:

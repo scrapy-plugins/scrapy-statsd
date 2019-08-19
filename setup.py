c0fa4a84da8cebd74f735ad0c44b967dda11dd61
mirceachira <chira.mircea.mc@gmail.com>
2019-08-19 20:03:57 +0300

    Fix setup.py and license

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+import pathlib
 from setuptools import setup, find_packages
 
 
@@ -6,14 +7,17 @@ setup(
     version='0.1.0',
     url='https://github.com/scrapy-plugins/scrapy-statsd',
     description='Scrapy extenstion to log stats to statsd',
+    long_description=(pathlib.Path(__file__).parent / 'README.md').read_text(),
+    long_description_content_type='text/markdown',
     author='Scrapy developers',
     license='BSD',
-    packages=find_packages(exclude=('tests', 'tests.*')),
-    include_package_data=True,
-    zip_safe=False,
-    install_requires=[
-        'Twisted>=16.0.0',
-        'Scrapy>=1.6.0',
-        'statsd>=3.3.0'
+    classifiers=[
+        'Framework :: Scrapy',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
     ],
+    packages=find_packages(exclude=('tests', 'tests.*')),
+    install_requires=['Twisted', 'Scrapy', 'statsd']
 )

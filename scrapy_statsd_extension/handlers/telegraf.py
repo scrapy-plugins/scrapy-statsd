40fd059d98cf458498f4436edda7775eead02e79
mirceachira <chira.mircea.mc@gmail.com>
2020-03-08 17:31:51 +0200

    telegraf handler

--- scrapy_statsd_extension/handlers/telegraf.py
+++ scrapy_statsd_extension/handlers/telegraf.py
@@ -1,12 +1,18 @@
-from telegraf.client import TelegrafClient
+import statsd
 
-from scrapy_statsd_extension import utils, defaults, handlers
+from scrapy_statsd_extension.handlers import StatsdBaseHandler
 
-class TelegrafHandler(handlers.StatsdBaseHandler):
 
+class TelegrafHandler(StatsdBaseHandler):
     def create_client(self, host, port, prefix):
-        return TelegrafClient(host, port, prefix)
+        return statsd.StatsClient(host, port, prefix)
+
+    def get_formatted_tags(self, spider):
+        tags = self.get_tags(spider)
+        return [f"{key}={value}" for key, value in tags.items()]
 
     def increment(self, key, value, spider):
+        formatted_tags = self.get_formatted_tags(spider)
+
         if self.not_ignored_field(key) and self.has_valid_prefix(key):
-            self.client.metric(key, value) #TODO: add tag support
+            self.client.incr(",".join([key, *formatted_tags]), value)

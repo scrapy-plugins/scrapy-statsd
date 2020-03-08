ea608b1eca679257173f14ccb9a79e0760532800
mirceachira <chira.mircea.mc@gmail.com>
2020-03-08 17:46:37 +0200

    fix graphite handler

--- scrapy_statsd_extension/handlers/graphite.py
+++ scrapy_statsd_extension/handlers/graphite.py
@@ -1,12 +1,20 @@
-import statsd
+import graphyte
 
-from scrapy_statsd_extension import handlers
+from scrapy_statsd_extension.handlers import StatsdBaseHandler
 
 
-class GraphiteHandler(handlers.StatsdBaseHandler):
+class GraphiteHandler(StatsdBaseHandler):
     def create_client(self, host, port, prefix):
-        return statsd.StatsClient(host, port, prefix)
+        import pudb
+
+        pu.db
+        graphyte.init(host, port=port, prefix=prefix)
+        return graphyte
 
     def increment(self, key, value, spider):
         if self.not_ignored_field(key) and self.has_valid_prefix(key):
-            self.client.incr(key, value)
+            import pudb
+
+            pu.db
+
+            self.client.send(key, value, tags=self.get_tags(spider))

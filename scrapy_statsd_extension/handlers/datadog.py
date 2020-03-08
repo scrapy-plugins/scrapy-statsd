a10f9b5077f7e0d3d7b44da3852ca664ec162e43
mirceachira <chira.mircea.mc@gmail.com>
2020-03-08 17:31:33 +0200

    datadog handler

--- scrapy_statsd_extension/handlers/datadog.py
+++ scrapy_statsd_extension/handlers/datadog.py
@@ -2,7 +2,7 @@ from os import environ
 
 from datadog import ThreadStats, initialize, statsd
 
-from scrapy_statsd_extension import handlers
+from scrapy_statsd_extension import handlers, defaults
 
 
 class DatadogHandler(handlers.StatsdBaseHandler):
@@ -10,19 +10,19 @@ class DatadogHandler(handlers.StatsdBaseHandler):
         super().__init__(crawler_settings)
 
         api_key = crawler_settings.get(
-            "STATSD_DDOG_API_KEY", environ.get("DATADOG_API_KEY")
+            "DATADOG_API_KEY", environ.get("DATADOG_API_KEY")
         )
-        app_key = crawler_settings.get(
-            "STATSD_DDOG_APP_KEY", environ.get("DATADOG_APP_KEY")
-        )
+        namespace = crawler_settings.get("STATSD_PREFIX", defaults.STATSD_PREFIX)
 
-        initialize(api_key=api_key, app_key=app_key)
+        initialize(api_key=api_key, statsd_namespace=namespace)
 
     def create_client(self, host, port, prefix):
-        stats_client = ThreadStats()
-        stats_client.start()
-        return stats_client
+        return statsd
+
+    def get_tags(self, spider):
+        tags = super().get_tags(spider)
+        return [f"{key}:{value}" for key, value in tags.items()]
 
     def increment(self, key, value, spider):
         if self.not_ignored_field(key) and self.has_valid_prefix(key):
-            self.client.gauge(key, value, tags=self.get_tags(spider))
+            self.client.increment(key, value, tags=self.get_tags(spider))
